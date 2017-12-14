2ed9f4477a4b1e0a411ab1963499bbfe47603620
lucas.g <lucas.garin73@gmail.com>
2017-12-14 15:34:06 +0100

    fix color

--- app/src/main/java/com/app/QuizzActivity.java
+++ app/src/main/java/com/app/QuizzActivity.java
@@ -13,20 +13,4 @@ public class QuizzActivity extends AppCompatActivity {
         setContentView(R.layout.activity_quizz);
     }
 
-
-    public void onClick(View view){
-        if (view.getId()==R.id.btn1){ // C'est notre bouton ? oui, alors affichage d'un message
-//            Intent intent = new Intent(FromActivity.this, ToActivity.class);
-//            startActivity(intent);
-        }
-        else if (view.getId()==R.id.btn2){ // C'est notre bouton ? oui, alors affichage d'un message
-            Toast.makeText(this,"Bouton 2", Toast.LENGTH_SHORT).show();
-        }
-        else if (view.getId()==R.id.btn3){ // C'est notre bouton ? oui, alors affichage d'un message
-            Toast.makeText(this,"Bouton 3", Toast.LENGTH_SHORT).show();
-        }
-        else if (view.getId()==R.id.btn4){ // C'est notre bouton ? oui, alors affichage d'un message
-            Toast.makeText(this,"Bouton 4", Toast.LENGTH_SHORT).show();
-        }
-    }
 }
