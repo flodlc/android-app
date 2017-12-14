c794d8d9adf2f66c4aea1678ca12c0e4e7f8f1d7
Florian DE LA COMBLE <f.delacomble@gmail.com>
2017-12-14 14:56:16 +0100

    wip

--- app/src/main/java/com/app/OfflineSelectorActivity.java
+++ app/src/main/java/com/app/OfflineSelectorActivity.java
@@ -1,10 +1,12 @@
 package com.app;
 
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.florian.android_app.R;
 
